a5f4ae930f9b9abc0364093e194268007e709aac
unknown <tienan_feng@126.com>
2020-07-24 09:49:58 +0800

    mythelation

---
+++ mythelation.R
@@ -0,0 +1,9 @@
+if (!requireNamespace("BiocManager", quietly=TRUE))
+install.packages("BiocManager")
+BiocManager::install("MEDIPS")
+library("BSgenome")
+available.genomes()
+BSgenome.Rnorvegicus.UCSC.rn5
+
+BiocManager::install("BSgenome.Hsapiens.UCSC.hg19")
+BiocManager::install("BSgenome.Hsapiens.UCSC.hg19")
